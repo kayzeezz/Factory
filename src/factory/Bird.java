be44c45c2f3925cb42d063cf41c28d5bbe811cd6
Kayzelle <130845069+kayzeezz@users.noreply.github.com>
2023-11-17 09:10:17 +0800

    Update and rename Tiger.java to Bird.java

---
+++ src/factory/Bird.java
@@ -0,0 +1,22 @@
+package factory;
+
+public class Bird implements  Animals{
+    private Integer Type;
+
+    public Integer getType() {
+        return Type;
+    }
+
+    public void setType(Integer Type) {
+        this.Type = Type;
+    }
+    @Override
+    public String makeSound() {
+        return "Tweet, tweet, tweet!";
+    }
+
+    @Override
+    public String play() {
+        return "attack moving objects!";
+    }
+}
