ea44599d89e2a76ee9637abbc06bc33192d456bc
Kayzelle <130845069+kayzeezz@users.noreply.github.com>
2023-11-17 08:46:53 +0800

    Update and rename Pet.java to Animal.java

---
+++ src/factory/Animal.java
@@ -0,0 +1,6 @@
+package factory;
+
+public interface Animals {
+    public String makeNoice();
+    public String wild();
+}
--- src/factory/Pet.java
+++
@@ -1,6 +0,0 @@
-package factory;
-
-public interface Pet {
-    public String makeSound();
-    public String play();
-}
